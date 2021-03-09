f3b4293648bce8b42bc1b069590a5b5159a18613
Thomas Visset <thmvisset31@gmail.com>
2021-03-09 16:52:23 +0100

    landing style

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :collections, only: [ :index, :show, :new, :create, :edit, :update, :destroy ] # Collections du user
-  resources :artworks, only: [ :index, :show, :new, :create, :edit, :update, :destroy ] # Artworks du user
+  resources :collections, only: [ :show, :new, :create, :edit, :update, :destroy ] # Collections du user
+  resources :artworks, only: [ :show, :new, :create, :edit, :update, :destroy ] # Artworks du user
 
   namespace :explore do # Explore all artworks from all users
     resources :artworks, only: [ :index ]
   end
 
-  resources :users, only: [] do # View other users profiles, collections or artworks
-    resources :collections, only: [ :index ], controller: 'users/collections'
-    resources :artworks, only: [ :index ], controller: 'users/artworks'
+  resources :users, only: [] do # View users profiles, collections or artworks
+    resources :collections, only: [ :index ]
+    resources :artworks, only: [ :index ]
   end
 end
