4edc3332a9bc405605b4d97a38f8464e259ba290
francois xavier paris <fxavierparis@gmail.com>
2021-03-10 15:44:06 +0100

    artwork creation ok

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,5 @@
 class ArtworksController < ApplicationController
+
   def index
   end
 
@@ -6,9 +7,14 @@ class ArtworksController < ApplicationController
   end
 
   def new
+    @artwork = Artwork.new
+    @artists = Artist.order(:name)
+    @collections = current_user.collections
   end
 
   def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.save
   end
 
   def edit
@@ -19,4 +25,11 @@ class ArtworksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :photo, :artist_id, :completion_year, :description, :notes, :collection_id)
+  end
+
 end
