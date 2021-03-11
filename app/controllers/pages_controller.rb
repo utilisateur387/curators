7e4eda33b0df48c8bc615a91f00caf80de93ce8c
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-03-11 22:11:46 +0100

    Explore cards on hp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     if current_user
       user = current_user
       @artwork_archive = user.artworks.where.not(artist_id: nil).sample
+      @artworks = Artwork.joins(:collection).where.not(collections: { user: current_user })
     end
   end
 end
