ff9be3a60fc6b84504dd05980dbe7777fed23588
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-03-12 14:02:34 +0100

    Explore reverse posts

--- app/controllers/explore/artworks_controller.rb
+++ app/controllers/explore/artworks_controller.rb
@@ -1,6 +1,6 @@
 class Explore::ArtworksController < ApplicationController
   def index
-    @artworks = Artwork.joins(:collection).where.not(collections: { user: current_user })
+    @artworks = Artwork.joins(:collection).where.not(collections: { user: current_user }).reverse()
   end
 
   def show
