6873a4d7c61b3447dae5ae3796024fac3092cd51
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-03-14 15:12:55 +0100

    wiki 1512

--- db/seeds/wikipedia_artists/wikipedia_artists.rb
+++ db/seeds/wikipedia_artists/wikipedia_artists.rb
@@ -17,34 +17,52 @@ html_doc.css('.div-col').css('li').each_with_index do |element, index|
   # puts element.element_children.first.attribute('href')
   # puts '_'
 
-  title = element.element_children.first.attribute('title')
+  title = element.element_children.first.attribute('title').text.strip
+  puts title
+  new_title = title.gsub(/[àáâãäå]/, "a")
+  new_title = new_title.gsub(/[ÀÁÂÄÆÃÅĀ]/, "A")
+  new_title = new_title.gsub(/[ôöòóøōõ]/, "o")
+  new_title = new_title.gsub(/[ÔÖÒÓØŌÕ]/, "O")
+  new_title = new_title.gsub(/[èéêëēėę]/, "e")
+  new_title = new_title.gsub(/[ÈÉÊËĒĖĘ]/, "E")
+  new_title = new_title.gsub(/[îïíīįì]/, "i")
+  new_title = new_title.gsub(/[ÎÏÍĪĮÌ]/, "i")
+  new_title = new_title.gsub(/[śš]/, "s")
+  new_title = new_title.gsub(/[ŚŠ]/, "S")
+  new_title = new_title.gsub(/[çćč]/, "c")
+  new_title = new_title.gsub(/[ÇĆČ]/, "C")
+  new_title = new_title.gsub(/[ÿ]/, "y")
+  new_title = new_title.gsub(/[Ÿ]/, "Y")
+  new_title = new_title.gsub(/[ûüùúū]/, "u")
+  new_title = new_title.gsub(/[ÛÜÙÚŪ]/, "U")
+  new_title = new_title.gsub(/[žźż]/, "z")
+  new_title = new_title.gsub(/[ŽŹŻ]/, "Z")
+  new_title = new_title.gsub(/[ñń]/, "n")
+  new_title = new_title.gsub(/[ÑŃ]/, "N")
+  # full_name = new_title.split.each { |word| word.capitalize! }.join(" ")
+  # puts full_name
   link = element.element_children.first.attribute('href')
 
-  url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=#{title}"
+  url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=#{new_title}"
   response = HTTParty.get(url)
   page_id = response["query"]["pages"].keys[0]
+  puts response["query"]["pages"][page_id]["extract"]
 end
 
 
+# url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Awataguchi Takamitsu"
+# response = HTTParty.get(url)
 
-
-url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Awataguchi Takamitsu"
-response = HTTParty.get(url)
-
-
-
-artists = response.map do |artist_data|
-  # puts artist_data
-  page_id = response["query"]["pages"].keys[0]
-  # puts "_"
-  # puts response["query"]["pages"][page_id]["extract"]
-  # puts
-  # Artwork.new(
-  #   title: artwork_data["title"],
-  #   completion_year: artwork_data["completitionYear"],
-  #   tmp_image_url: artwork_data["image"],
-  #   tmp_artist_name: artwork_data["artistName"]
-  # )
-end
-
+# artists = response.map do |artist_data|
+#   page_id = response["query"]["pages"].keys[0]
+#   # puts response["query"]["pages"][page_id]["extract"]
+#   # puts "_"
+#   # puts
+#   # Artwork.new(
+#   #   title: artwork_data["title"],
+#   #   completion_year: artwork_data["completitionYear"],
+#   #   tmp_image_url: artwork_data["image"],
+#   #   tmp_artist_name: artwork_data["artistName"]
+#   # )
+# end
 
