1eed77732c5f35a9bfdf37f83c46feb90e0813f9
TEFFAUD Hippolyte <hippolyte.teffaud@essca.eu>
2021-03-18 11:05:48 +0100

    final anim

--- app/javascript/components/scroll_reveal.js
+++ app/javascript/components/scroll_reveal.js
@@ -1,6 +1,7 @@
 const scrollReveal = () => {
-  ScrollReveal().reveal('.card-text-over-img', { delay: 140 });
-  ScrollReveal().reveal('.explore-top-infos', { delay: 190 });
+  ScrollReveal().reveal('.card-text-over-img', { opacity: 0, distance: '50px',delay: 140 });
+  ScrollReveal().reveal('.explore-top-infos', { opacity: 0, distance: '50px', delay: 190 });
+  ScrollReveal().reveal('.explore-all-artworks', { opacity: 0, distance: '50px', delay: 190 });
 };
 
 export { scrollReveal };
